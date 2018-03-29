1df35454eba00e2072177b3168e0d7b0680867df
dudyrev.aa <dudyrev.aa@parma.ru>
2018-03-29 15:11:38 +0500

    Initial Commit

---
+++ cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus
+//
+//  Created by Александр on 29.03.2018.
+//  Copyright © 2018 Александр. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
